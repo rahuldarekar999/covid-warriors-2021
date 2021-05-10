c4f1ec07841fe6857179a7b50717300cc8b0c1dc
Rahul Darekar <rahuldarekar999@gmail.com>
2021-05-10 18:30:48 +0530

    search text with sub category

--- src/main/java/com/covid/warriors/service/impl/CovidWarriorServiceImpl.java
+++ src/main/java/com/covid/warriors/service/impl/CovidWarriorServiceImpl.java
@@ -743,7 +743,8 @@ public class CovidWarriorServiceImpl implements CovidWarriorsService {
 
 	@Override
 	public void saveDataForSentMessagesFromSocialMedia(CustomMessage customMessage) {
-		Set<String> contacts = dataScraperService.scrapeDataFromTwitterUrl(customMessage.getCity(), customMessage.getCategory());
+		String searchText = StringUtils.isNotBlank(customMessage.getSubCat()) ? customMessage.getCategory() + " " + customMessage.getSubCat() : customMessage.getCategory();
+		Set<String> contacts = dataScraperService.scrapeDataFromTwitterUrl(customMessage.getCity(), searchText);
 		if(!CollectionUtils.isEmpty(contacts)) {
 			customMessage.setMobileList(new ArrayList<String>(contacts));
 			try {
