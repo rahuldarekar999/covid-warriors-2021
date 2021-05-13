f3c357835eb9afd585dd9453253d59b41fb32898
Rahul Darekar <rahuldarekar999@gmail.com>
2021-05-13 18:00:21 +0530

    send notification on response received

--- src/main/java/com/covid/warriors/service/MessageResponseService.java
+++ src/main/java/com/covid/warriors/service/MessageResponseService.java
@@ -1,16 +1,20 @@
 package com.covid.warriors.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import com.covid.warriors.entity.model.MessageResponseEntity;
 import com.covid.warriors.repository.MessageResponseRepository;
 import com.covid.warriors.service.impl.UrlService;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class MessageResponseService {
@@ -18,9 +22,18 @@ public class MessageResponseService {
     @Autowired
     private UrlService urlService;
 
+    @Value("${sms.reply.message}")
+	private String smsReplyMessage;
+    
+    @Value("#{'${valid.sms.response.list}'.split(',')}")
+	private List<String> validAnswers;
+    
     @Autowired
     private MessageResponseRepository messageResponseRepository;
 
+    @Autowired
+	private CovidWarriorsSmsService covidWarriorSmsServiceImpl;
+    
     public String saveMessageResponse(String param) {
         try {
             String decodedParams = urlService.getOriginalUrl(param);
@@ -49,6 +62,16 @@ public class MessageResponseService {
             messageResponseEntity.setMessage(paramsMap.get("a"));
             messageResponseEntity.setMobile(paramsMap.get("m"));
             messageResponseEntity.setCreatedAt(LocalDateTime.now());
+            
+            if(StringUtils.isNotBlank(messageResponseEntity.getMessage()) && 
+            		validAnswers.contains(messageResponseEntity.getMessage().toLowerCase())) {
+	            String msg = smsReplyMessage;
+	            msg = msg.replace("!cat!", messageResponseEntity.getCategory()).replace("!mob!", messageResponseEntity.getMobile())
+	            		.replace("!city!", messageResponseEntity.getCity());
+	            List<String> mobList = new ArrayList<>();
+	            mobList.add(messageResponseEntity.getMobile());	            
+	            covidWarriorSmsServiceImpl.sendSms(mobList, msg);
+            }
             return messageResponseEntity;
         }
         return null;
