d904d6daa251ba6d78ee3eeacaac0bc012c01196
Rahul Darekar <rahuldarekar999@gmail.com>
2021-05-17 19:09:21 +0530

    insta hashtag prep

--- src/main/java/com/covid/warriors/controller/InstagramDataController.java
+++ src/main/java/com/covid/warriors/controller/InstagramDataController.java
@@ -1,13 +1,15 @@
 package com.covid.warriors.controller;
 
-import com.covid.warriors.request.model.FeedbackRequest;
-import com.covid.warriors.service.InstagramScraperService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.covid.warriors.service.InstagramScraperService;
 
 @RestController
 @CrossOrigin
@@ -18,6 +20,6 @@ public class InstagramDataController {
 
     @RequestMapping(value = "/data/insta", method = RequestMethod.GET)
     public Set<String> getNumbersFromInstagram(@RequestParam String city, @RequestParam String category) {
-        return instagramScraperService.getContactsFromInstagram(city, category);
+        return instagramScraperService.getContactsFromInstagram(city.toLowerCase(), category.toLowerCase());
     }
 }
--- src/main/java/com/covid/warriors/service/InstagramScraperService.java
+++ src/main/java/com/covid/warriors/service/InstagramScraperService.java
@@ -1,54 +1,71 @@
 package com.covid.warriors.service;
 
-import com.covid.warriors.response.model.instagram.Edge;
-import com.covid.warriors.response.model.instagram.InstagramScraperResponse;
-import com.covid.warriors.response.model.ocr.OcrResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.i18n.phonenumbers.PhoneNumberMatch;
-import com.google.i18n.phonenumbers.PhoneNumberUtil;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import com.covid.warriors.response.model.instagram.InstagramScraperResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.i18n.phonenumbers.PhoneNumberMatch;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @Service
 public class InstagramScraperService {
 
-    private static final String instagramUrl = "https://instagram47.p.rapidapi.com/hashtag_post?hashtag=%s%s";
+    private static final String instagramUrl = "https://instagram47.p.rapidapi.com/hashtag_post?hashtag=%s";
 
     private static final String rapidApiKey = "8396e3720cmsh83d0c4ae187994dp1797dejsn5be9e80c2006";
 
     private static final String rapidApiHost = "instagram47.p.rapidapi.com";
 
-    public static void main(String[] args) throws JsonProcessingException {
-
-    }
-
     public Set<String> getContactsFromInstagram(String city, String category) {
         try {
-            InstagramScraperResponse scraperResponse = getPostsFromInstagram(city, category);
+        	List<String> hashtags = new ArrayList<>();
+    		
+    		hashtags.add(city.concat(category));
+    		hashtags.add(category.concat("in").concat(city));
+    		hashtags.add(city.concat(category).concat("supplier"));
+    		hashtags.add(category.concat("supplierin").concat(city));
+    		Set<String> set = new HashSet<>();
+    		hashtags.forEach(hashtag -> {
+    			 InstagramScraperResponse scraperResponse = getPostsFromInstagram(hashtag);
+    	            if(Objects.nonNull(scraperResponse) && Objects.nonNull(scraperResponse.getBody())
+    	                    && Objects.nonNull(scraperResponse.getBody().getEdgeHashtagToMedia())
+    	                    && Objects.nonNull(scraperResponse.getBody().getEdgeHashtagToMedia().getEdges())) {
+    	                extractPhoneNumber(scraperResponse, set);
+    	            }
+    		});
+    		return set;
+            /*InstagramScraperResponse scraperResponse = getPostsFromInstagram(city, category);
             if(Objects.nonNull(scraperResponse) && Objects.nonNull(scraperResponse.getBody())
                     && Objects.nonNull(scraperResponse.getBody().getEdgeHashtagToMedia())
                     && Objects.nonNull(scraperResponse.getBody().getEdgeHashtagToMedia().getEdges())) {
                 Set<String> set = new HashSet<>();
                 extractPhoneNumber(scraperResponse, set);
                 return set;
-            }
+            }*/
         } catch (Exception e) {
             e.printStackTrace();
         }
         return Collections.emptySet();
     }
 
-    public InstagramScraperResponse getPostsFromInstagram(String city, String category) {
-        try {
+    public InstagramScraperResponse getPostsFromInstagram(String hashtag) {
+    	try {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
-                    .url(String.format(instagramUrl, category.toLowerCase(), city.toLowerCase()))
+                    .url(String.format(instagramUrl, hashtag))
                     .get()
                     .addHeader("x-rapidapi-key", rapidApiKey)
                     .addHeader("x-rapidapi-host", rapidApiHost)
@@ -62,7 +79,7 @@ public class InstagramScraperService {
         return null;
     }
 
-    private static Set<String> extractPhoneNumber(InstagramScraperResponse ocrResponse, Set<String> phoneNumbers) {
+    private static void extractPhoneNumber(InstagramScraperResponse ocrResponse, Set<String> phoneNumbers) {
         if(!ocrResponse.getBody().getEdgeHashtagToMedia().getEdges().isEmpty()) {
             ocrResponse.getBody().getEdgeHashtagToMedia().getEdges().forEach(edge -> {
                 Iterator<PhoneNumberMatch> existsPhone= PhoneNumberUtil.getInstance().findNumbers(edge.getNode().getAccessibilityCaption(),
@@ -76,7 +93,20 @@ public class InstagramScraperService {
                 }
             });
         }
-        return Collections.emptySet();
+    //    return Collections.emptySet();
     }
+    
+    public static void main(String[] args) {
+		String city="CHENNAI".toLowerCase();
+		String category="OXYGEN".toLowerCase();
+		List<String> hashtags = new ArrayList<>();
+		
+		hashtags.add(city.concat(category));
+		hashtags.add(category.concat("in").concat(city));
+		hashtags.add(city.concat(category).concat("supplier"));
+		hashtags.add(category.concat("supplierin").concat(city));
+		
+		System.out.println(hashtags);
+	}
 
 }
